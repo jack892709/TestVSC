1c8d60086f26835630c78a4f0a772cb868302b44
jack892709 <jack892709@gmail.com>
2020-10-22 15:08:05 +0800

    testing twig

--- testVSC.cpp
+++ testVSC.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {    
     cout << "Devil Number:" << GetDevilNumber() << endl;
+    cout << "A branch is bigger than a twig.\n";
 }
 
 int GetDevilNumber()
